3924e964aaac9aa54203de6470fdcefa4c9d57a6
Stevenh16 <sdnavarros@unimagdalena.edu.co>
2025-09-06 12:42:51 -0500

    Eliminación de mención inncesaria a umbra:: y corrección del nombre del metodo

--- src/preprocessor/Sanitize.cpp
+++ src/preprocessor/Sanitize.cpp
@@ -3,21 +3,21 @@
 #include<string>
 namespace umbra {
 
-    Encoding Sanitizer::detectEncoding(std::string& inputBytes){
+    Encoding Sanitizer::detectEnconding(std::string& inputBytes){
 	if(inputBytes.size() >= 3){
 		if((unsigned char)inputBytes[0] == 0xEF && (unsigned char)inputBytes[1] == 0xBB && (unsigned char)inputBytes[2] == 0xBF){
-			return umbra::Encoding::UTF8;
+			return Encoding::UTF8;
 		}
 
 		if((unsigned char)inputBytes[0] == 0xFE && (unsigned char)inputBytes[1] == 0xFF){
-			return umbra::Encoding::UTF16BE;
+			return Encoding::UTF16BE;
 		}
 
 		if((unsigned char)inputBytes[0] == 0xFF && (unsigned char)inputBytes[1] == 0xFE){
-			return umbra::Encoding::UTF16LE;
+			return Encoding::UTF16LE;
 		}	
 	}
-	return umbra::Encoding::UNKNOWN;
+	return Encoding::UNKNOWN;
     }	    
 	
     std::string Sanitizer::convertInternalEncoding(std::string& inputBytes){
